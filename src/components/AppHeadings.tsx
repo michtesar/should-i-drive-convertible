5c09c52336c3c3516961eab6aa9ecfe626cbb7c9
Michael Tesar <michtesar@gmail.com>
2023-05-24 17:08:49 +0200

    Rebranding

--- src/components/AppHeadings.tsx
+++ src/components/AppHeadings.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export const AppHeadings = () => {
     return (
         <React.Fragment>
-            <h1>Should I drive convertible today?</h1>
+            <h1>Should I lower the roof now?</h1>
         </React.Fragment>
     )
 }
