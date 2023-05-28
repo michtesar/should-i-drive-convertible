63fb1ce34833ab5cbe557722803f789de954ff94
Michael Tesar <michtesar@gmail.com>
2023-05-28 14:54:30 +0200

    Add better headings

--- src/components/AppHeadings.tsx
+++ src/components/AppHeadings.tsx
@@ -1,9 +1,11 @@
 import React from "react"
+import {Stack, Typography} from "@mui/material";
 
 export const AppHeadings = () => {
     return (
-        <React.Fragment>
-            <h1>Should I lower the roof now?</h1>
-        </React.Fragment>
+        <Stack spacing={2} marginTop={'5%'}>
+            <Typography variant={'h2'}>Should I lower the roof <strong>now</strong>?</Typography>
+            <Typography variant={'body2'}>...in the next 4 hours</Typography>
+        </Stack>
     )
 }
