8bee53ea024accfb29113578516bc577e2ae6307
Michael Tesar <michtesar@gmail.com>
2023-05-28 15:11:54 +0200

    Simplify the headings

--- src/components/AppHeadings.tsx
+++ src/components/AppHeadings.tsx
@@ -4,7 +4,7 @@ import {Stack, Typography} from "@mui/material";
 export const AppHeadings = () => {
     return (
         <Stack spacing={2} margin={'5%'}>
-            <Typography variant={'h2'}>Should I lower the roof <strong>now</strong>?</Typography>
+            <Typography variant={'h2'}>Should I lower the roof?</Typography>
         </Stack>
     )
 }
