1d97f261248fc40cb581aec13952f529380e7558
Michael Tesar <michtesar@gmail.com>
2023-05-28 15:05:59 +0200

    Add 5% margin to the heading

--- src/components/AppHeadings.tsx
+++ src/components/AppHeadings.tsx
@@ -3,9 +3,8 @@ import {Stack, Typography} from "@mui/material";
 
 export const AppHeadings = () => {
     return (
-        <Stack spacing={2} marginTop={'5%'}>
+        <Stack spacing={2} margin={'5%'}>
             <Typography variant={'h2'}>Should I lower the roof <strong>now</strong>?</Typography>
-            <Typography variant={'body2'}>...in the next 4 hours</Typography>
         </Stack>
     )
 }
