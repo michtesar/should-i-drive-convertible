06068de2a784008c8f6b147284a0f36d7b28ffaf
Michael Tesar <michtesar@gmail.com>
2023-05-28 16:08:51 +0200

    Make Heading smaller

--- src/components/AppHeadings.tsx
+++ src/components/AppHeadings.tsx
@@ -3,8 +3,8 @@ import {Stack, Typography} from "@mui/material";
 
 export const AppHeadings = () => {
     return (
-        <Stack spacing={2} margin={'5%'}>
-            <Typography variant={'h2'}>Should I lower the roof?</Typography>
+        <Stack margin={'5%'}>
+            <Typography variant={'h3'}>Should I lower the roof?</Typography>
         </Stack>
     )
 }
