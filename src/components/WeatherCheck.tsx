fc5526abcfaa411c307f019c48c0584059422fb2
Michael Tesar <michtesar@gmail.com>
2023-05-28 15:20:30 +0200

    Add icons to the overview

--- src/components/WeatherCheck.tsx
+++ src/components/WeatherCheck.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import {LimitCheck} from "./DetailView";
 import "../App.css"
 import {Stack, Typography} from "@mui/material";
+import {FcCancel, FcCheckmark} from "react-icons/fc";
 
 interface WeatherCheckInterface {
     name: string
@@ -19,6 +20,7 @@ export const WeatherCheck = ({name, units, limit, threshold}: WeatherCheckInterf
                     <Typography variant={'body2'}>Limit is <strong>{threshold} {units}</strong></Typography>
                     <Typography variant={'body2'}>Average
                         from <strong>{limit.min}</strong> to <strong>{limit.max}</strong> {units}</Typography>
+                    <FcCancel size={28}/>
                 </Stack>
             }
             {limit.ok &&
@@ -26,6 +28,7 @@ export const WeatherCheck = ({name, units, limit, threshold}: WeatherCheckInterf
                     <Typography variant={'body2'}>Limit is <strong>{threshold}</strong> {units}</Typography>
                     <Typography variant={'body2'}>Average <strong>{limit.average.toFixed(0)}</strong> {units}
                     </Typography>
+                    <FcCheckmark size={28}/>
                 </Stack>
             }
         </div>
